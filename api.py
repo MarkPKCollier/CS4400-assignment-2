53e5b23498f68f880423b22e7313b87508657f52
Mark Collier <markcollier@cs.uchicago.edu>
2017-11-26 21:12:12 +0000

    Returning results as json

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import request
+from flask import jsonify
 from subprocess import call, check_output
 from git import Repo
 
@@ -13,7 +14,10 @@ def api():
     max_workers = request.args.get('max_workers')
     call(['git clone {0} {1}'.format(git_url, repo_dest)], shell=True)
     complexity = check_output(['mpiexec -n {0} python work_stealing.py --repo_dir={1}'.format(int(max_workers)+1, repo_dest)], shell=True)
-    return git_url, complexity
+    return jsonify({
+        'git_url': git_url,
+        'complexity': int(complexity.strip()) if isinstance(complexity, str) else complexity
+        })
 
 if __name__ == "__main__":
     app.run()
--- test_client.py
+++ test_client.py
@@ -1,5 +1,4 @@
 import requests
 
 r = requests.get('http://127.0.0.1:5000/?git_url=https://github.com/rubik/argon.git&max_workers=4')
-print r
 print r.content
